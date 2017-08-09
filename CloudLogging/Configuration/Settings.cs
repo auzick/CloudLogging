bdb4a3e2f0ba3517a187a89d6640b43311f7fbfe
Andy Uzick <andy@uzick.com>
2017-08-09 09:47:54 -0500

    Changing MaxLogBlocks

--- CloudLogging/Configuration/Settings.cs
+++ CloudLogging/Configuration/Settings.cs
@@ -3,6 +3,6 @@
     public static class Settings
     {
         public const double MaxLogSize = 1e+7;
-        public const int MaxLogBlocks = 49000;
+        public const int MaxLogBlocks = 49900;
     }
 }
